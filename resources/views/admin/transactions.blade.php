12aa89df61f6c41ce9670817342f6acb1a7adc4c
unknown <fathur.risqo@gmail.com>
2025-10-23 11:02:07 +0700

    Change document title on admin dashboard

--- resources/views/admin/transactions.blade.php
+++ resources/views/admin/transactions.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <title>Dashboard | Transaction</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
